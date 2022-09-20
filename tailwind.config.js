15e2df98defc98d40d226ee2e584155146b8176a
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-09-20 16:06:33 +0530

    modified tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      "break-1": "1375px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          1: "#EDF6FF",
+          2: "#303F60",
+          3: "#1A253C",
+          4: "#43AFFF",
+          5: "#43AFFF33",
+          6: "#4D618E",
+          7: "#557DA526",
+          8: "#FF0000",
+          9: "#FF333380",
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
